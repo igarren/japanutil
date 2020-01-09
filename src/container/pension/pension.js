b7d9c3ea28db4acf89519bc8ab2141f5b38f9c68
scatalinio@gmail.com <scatalinio@gmail.com>
2020-01-09 04:24:07 -0500

    - downgraded @date-io/date-fns - fixed ui

--- src/container/pension/pension.js
+++ src/container/pension/pension.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import 'date-fns';
 import InputLabel from '@material-ui/core/InputLabel';
 import TextField from '@material-ui/core/TextField';
 import Container from '@material-ui/core/Container';
@@ -6,6 +7,11 @@ import IconButton from '@material-ui/core/IconButton';
 import Button from '@material-ui/core/Button';
 import HelpIcon from '@material-ui/icons/Help';
 import SelectCommon from '../../components/ui/select/SelectCommon';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
 import './pension.css';
 
 
@@ -18,8 +24,14 @@ export default class MainBody extends Component {
     this.state = {
       error: null,
       isLoaded: false,
-      isCalculating: false
+      isCalculating: false,      
+      category: null,
+      type: null,
+      length: null,
+      lastContribution: null, 
+      items: null
     };
+    
   }
   
   CalcOutput() {
@@ -36,21 +48,73 @@ export default class MainBody extends Component {
   
   const changeCategory = event => {
     console.log(event.target.value);
-    //setCategory(event.target.value);
+    this.setState({
+      category: event.target.value,
+    });
   };
   const changeType = event => {
     console.log(event.target.value);
-    //setCategory(event.target.value);
+    this.setState({
+      type: event.target.value,
+    });
+  };
+        
+  const changeYearsOfStay = event => {
+    
+    console.log(event.target.value);
+    this.setState({
+      length: event.target.value,
+    });
+  }
+  
+  const changeLastContribution = event => {
+    
+    console.log(event.target.value);
+    this.setState({
+      lastContribution: event.target.value,
+    });
   };
   
   const doCalculate = event => {
+    const { category, type, length, lastContribution } = this.state;
+    
     this.state.isCalculating = true;
     this.setState({
       isCalculating: true,
     });
+    fetch("https://services.ltdrive.net/public/pension/calculate",
+         {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body:{
+              category: category,
+              length: length,
+              type: type,
+            }
+        }
+      )
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.result,
+            isCalculating: false
+          });
+        },        
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
     console.log("calculating");
   };
-  
+        
+        
   const categoryList = [
       {value : 'cat_1', text : 'Category I'},
       {value : 'cat_2', text : 'Category II'},
@@ -79,7 +143,9 @@ export default class MainBody extends Component {
             <IconButton  color="primary">
               <HelpIcon />
             </IconButton>
-            <TextField label="Years of Stay" id="input-years"/>
+            <TextField label="Years of Stay" id="input-years"              
+              onChange={changeYearsOfStay}
+            />
           </div>    
           <div>
             <SelectCommon
@@ -92,6 +158,21 @@ export default class MainBody extends Component {
               <HelpIcon />
             </IconButton>
           </div>    
+    
+          <div>
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <KeyboardDatePicker
+                    margin="normal"
+                    id="input-last-contribution"
+                    label="Last Contribution"
+                    format="yyyy/MM/dd"
+                    KeyboardButtonProps={{
+                      'aria-label': 'change date',
+                    }}
+                    onChange={changeLastContribution}
+                  />
+             </MuiPickersUtilsProvider>
+          </div>
           <div className="Pension-container-action"> 
             <Button variant="contained" color="primary"
                onClick={doCalculate}
